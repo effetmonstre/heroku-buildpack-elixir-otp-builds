d5c4a9559602a2b8665db7fefa4b0ceabfd3f712
Mathieu Cote <mathieu@effetmonstre.com>
2025-02-17 21:29:54 -0500

    fixed path for ubuntu 24.04

--- build-otp.sh
+++ build-otp.sh
@@ -23,8 +23,9 @@ else
   make release
 
   cd ../
-  mv otp-OTP-${OTP_VERSION}/release/x86_64-unknown-linux-gnu/ OTP-${OTP_VERSION}
-  # mv otp-OTP-${OTP_VERSION}/release/aarch64-unknown-linux-gnu/ OTP-${OTP_VERSION}
+  # mv otp-OTP-${OTP_VERSION}/release/x86_64-unknown-linux-gnu/ OTP-${OTP_VERSION}
+  mv otp-OTP-${OTP_VERSION}/release//x86_64-pc-linux-gnu/ OTP-${OTP_VERSION} # Ubuntu 24.04
+  # mv otp-OTP-${OTP_VERSION}/release/aarch64-unknown-linux-gnu/ OTP-${OTP_VERSION} #OSX
   rm OTP-${OTP_VERSION}.tar.gz
   tar -zcf out/OTP-${OTP_VERSION}.tar.gz OTP-${OTP_VERSION}
 fi
